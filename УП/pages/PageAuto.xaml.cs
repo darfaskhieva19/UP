b7f00c84971e19fd17e2406ca5d2575539aaf8b1
Дарья Фасхиева <dashaf712@mail.ru>
2023-03-14 14:33:44 +0300

    корректировки

--- УП/pages/PageAuto.xaml.cs
+++ УП/pages/PageAuto.xaml.cs
@@ -25,10 +25,13 @@ namespace УП
         DispatcherTimer timer = new DispatcherTimer();
         int time = 10;
         string code;
+        string str = String.Empty;
+
 
         public PageAuto()
         {
             InitializeComponent();
+            tbNumber.Focus();
         }
        
         private void btnOtm_Click(object sender, RoutedEventArgs e) //кнопка отмены
@@ -41,20 +44,36 @@ namespace УП
         }
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
-            
+            if(tbCode.Text == code)
+            {
+                timer.Stop();
+                tbTime.Text = "";
+                MessageBox.Show($"Вы успешно авторизовались! Ваша роль - {employee.Roles.Role}");
+            }
+            else
+            {
+                timer.Stop();
+                tbTime.Text = "";
+                MessageBox.Show("Неверный код!");
+                tbCode.Text = "";
+                btnUpCode.IsEnabled = false;
+                btnEnter.IsEnabled = false;
+                tbCode.IsEnabled = false;
+            }
         }
         private void tbNumber_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                if (DataBase.Base.Employees.FirstOrDefault(z => z.Number == tbNumber.Text) != null)
+                Employees employees = DataBase.Base.Employees.FirstOrDefault(z => z.Number == tbNumber.Text);
+                if (employees != null)
                 {
-                    employee = DataBase.Base.Employees.FirstOrDefault(z => z.Number == tbNumber.Text);
                     tbpass.IsEnabled = true;
                     tbpass.Focus();
                 }
                 else
                 {
+                    tbpass.IsEnabled = false;
                     MessageBox.Show("Нет такого номера!");
                 }
             }
@@ -63,20 +82,32 @@ namespace УП
         {
             if (e.Key == Key.Enter)
             {
-                if (employee.Password == tbpass.Password.GetHashCode())
-                {                    
+                int pp = tbpass.Password.GetHashCode();
+                Employees employees = DataBase.Base.Employees.FirstOrDefault(z => z.Password == pp);
+                if (employee != null)
+                {
                     tbCode.IsEnabled = true;
-                    timer.Interval = new TimeSpan(0, 0, 10);
+                    timer.Interval = new TimeSpan(0, 0, 1);
                     timer.Tick += new EventHandler(Timer_Tick);
-                    tbCode.Focus();                    
+                    Time();
                 }
                 else
                 {
-                    MessageBox.Show("Неправильный пароль");
+                    MessageBox.Show("Введенный пароль неверный!");
                 }
-
             }
         }
+
+        public void Time()
+        {
+            string code = GenerateCode();
+            MessageBox.Show(code);
+            tbCode.Focus();
+            time = 10;
+            timer.Start();
+            tbTime.Text = "10 секунд";
+        }
+
         private void Timer_Tick(object sender, EventArgs e)
         {
             time--;
@@ -85,12 +116,12 @@ namespace УП
             {
                 timer.Stop();
                 tbTime.Text = "";
-                MessageBox.Show("Вы не успели ввести код.Сгенирируйте заново");
+                MessageBox.Show("Вы не успели ввести код.Сгенирируйте код заново!");
                 tbCode.Text = "";
                 btnUpCode.IsEnabled = true;
-            }
-            tbCode.IsEnabled = false;
-            btnEnter.IsEnabled = false;
+                tbCode.IsEnabled = false;
+                btnEnter.IsEnabled = false;
+            }            
         }
 
         private void tbNumber_PreviewTextInput(object sender, TextCompositionEventArgs e) //запрет на символы
@@ -101,27 +132,25 @@ namespace УП
             }
         }
 
-        private void tbCode_PreviewTextInput(object sender, TextCompositionEventArgs e) //запрет на символы
-        {
-            if (!(Char.IsDigit(e.Text, 0)))
-            {
-                e.Handled = true;
-            }
-        }
-
         private void btnUpCode_Click(object sender, RoutedEventArgs e)
         {
-            //Random rnd = new Random();
-            //int count = rnd.Next(10000, 50000);
-            //string str = "";
-            //string sym = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            //string sp = "!#$()*+,-./;<=>?@[]^_{|}";
-            //for(int i=0; str.Length < 8; i++)
-            //{
-            //    str += sym[rnd.Next(sym.Length)] + sp[rnd.Next(sp.Length)];
-            //}
-            //MessageBox.Show(str + "\nЗапомните пожалуйста код", "Код");
+            btnUpCode.IsEnabled = false;
+            tbCode.IsEnabled = true;
+            Time();
+        }
 
+        public string GenerateCode()
+        {
+            code = "";
+            str = string.Empty;
+            Random rnd = new Random();
+            string sym = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$()*+,-./;<=>?@[]^_{|}";           
+            for(int i = 0; i < 8; i++)
+            {
+                str += sym[rnd.Next(sym.Length)];
+            }
+            return str;
+            MessageBox.Show(str + "\nЗапомните пожалуйста код", "Код");
         }
     }
 }
